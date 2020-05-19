2963aa25e7646ecb46f67cdad1d4c13d10e9c0db
zachariapopcorn <52849920+zachariapopcorn@users.noreply.github.com>
2020-05-18 21:30:10 -0700

    Update loginstatus.js

--- loginstatus.js
+++ loginstatus.js
@@ -9,15 +9,3 @@ exports.run = async (client, message, args) => {
     const data = await roblox.getCurrentUser();
     return message.channel.send(`You are logged in to the Roblox account **${data.UserName}** that has the user id of **${data.UserID}**!`);
 }
-
-exports.help = (client, message, args) => {
-    message.channel.send({embed: {
-        color: 7948427,
-        description: `**Login Status**\n`
-        + `\`${client.config.prefix}loginstatus\` - Logs into a Roblox account using the supplied username and password (Onwer Only).`,
-        author: {
-            name: message.author.tag,
-            icon_url: message.author.displayAvatarURL()
-        }
-    }});
-}
